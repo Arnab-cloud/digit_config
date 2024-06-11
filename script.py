200252fd4f90cb12a9631d91a475bc947b5eb062
Arnab <arnabsantra248@gmail.com>
2024-06-11 19:19:27 +0530

    Added CrossEntropyCost, Epoch 29: 9435/10000, took 8.96 seconds

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
-import network
+import network2 as network
 import mnist_loader
 
 net = network.Network([784, 30, 10])
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+net.SGD(training_data, 30, 10, 0.5, test_data=test_data)
